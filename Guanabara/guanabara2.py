96314589a741f0f9ca5e8e82e9b1cb72bff2454a
math3us2021 <88463383+math3us2021@users.noreply.github.com>
2021-11-16 19:32:15 -0300

    Create guanabara2.py

---
+++ Guanabara/guanabara2.py
@@ -0,0 +1,8 @@
+from math import ceil
+nome = input("Qual o seu nome: ")
+print("Prazer em conhece-ló {}".format(nome))
+n1 = int(input("Digite um numero para ver sua medida em cm: "))
+cm = n1 * 100
+print("O valor {} em em sentimetros é: {} cm ".format (n1 , cm ))
+
+
