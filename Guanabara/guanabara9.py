77dd297afd06e193c731678bfafe063c406d55c4
math3us2021 <88463383+math3us2021@users.noreply.github.com>
2021-11-16 19:32:57 -0300

    Create guanabara9.py

---
+++ Guanabara/guanabara9.py
@@ -0,0 +1,10 @@
+def lin (msg):  # colocando o def cria a função.. ela pode ficar vazia, ou incluir algum escrito posterior
+    print('--' *30)
+    print(msg)
+    print('--' *30)
+    
+    
+lin( 'Python é mais fácil') # foi incluido um escrito, que substituio o 'msg'
+    
+     
+    
