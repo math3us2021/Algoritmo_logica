5e80b82707ac04b0b700279fa925fdae80f72cbf
math3us2021 <88463383+math3us2021@users.noreply.github.com>
2021-11-16 19:33:12 -0300

    Delete guanabara1.py

--- python/guanabara1.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input('Digite um numero: '))
-ns = float(input("Digite outro numero: "))
-s = n1 + ns
-print("A soma vale", s)
-#print("Asoma entre",n1,"e",ns,"vale: ", s )
-print("A soma entre {} e {} vale {}".format(n1, ns, s))
-print(type(s))
-print(5*5)
